50f56bb7564f4890c2a23b1bab59b98610c36bde
unknown <Administrator@claudia-pc.temasoft.pub>
2015-10-08 09:12:38 +0300

    Lotto problem

---
+++ PbPart3/LotoTests/LotoTests.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PbPart3;
+
+namespace LotoTests
+{
+    [TestClass]
+    public class LotoTests
+    {
+        [TestMethod]
+        public void Odds_For_Lotto_2_of_5()
+        {
+            long numbersToBeGuessed = 2;
+            long totalNumbers = 5;
+
+            long odds = PbPart3.Loto.CalculateOdds(numbersToBeGuessed, totalNumbers);
+
+            Assert.AreEqual("10", odds.ToString(), "The odds are not calculated correctly");
+        }
+
+        [TestMethod]
+        public void Odds_For_Lotto_6_of_49()
+        {
+            long numbersToBeGuessed = 6;
+            long totalNumbers = 49;
+
+            long odds = PbPart3.Loto.CalculateOdds(numbersToBeGuessed, totalNumbers);
+
+            Assert.AreEqual("13983816", odds.ToString(), "The odds are not calculated correctly");
+        }
+
+        [TestMethod]
+        public void Odds_For_Lotto_5_of_49()
+        {
+            long numbersToBeGuessed = 5;
+            long totalNumbers = 49;
+
+            long odds = PbPart3.Loto.CalculateOdds(numbersToBeGuessed, totalNumbers);
+
+            Assert.AreEqual("1906884", odds.ToString(), "The odds are not calculated correctly");
+        }
+
+        [TestMethod]
+        public void Odds_For_Lotto_4_of_49()
+        {
+            long numbersToBeGuessed = 4;
+            long totalNumbers = 49;
+
+            long odds = PbPart3.Loto.CalculateOdds(numbersToBeGuessed, totalNumbers);
+
+            Assert.AreEqual("211876", odds.ToString(), "The odds are not calculated correctly");
+        }
+    }
+}
---
+++ PbPart3/PbPart3/Loto.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PbPart3
+{
+    public class Loto
+    {
+        public static long CalculateOdds(long numbersToBeGuessed, long totalNumbers)
+        {
+            long numarator = CalculateNumerator(totalNumbers, numbersToBeGuessed);
+
+            long odds = numarator / CalculatePermutations(numbersToBeGuessed);
+
+            return odds;
+        }
+
+        public static long CalculatePermutations(long n)
+        {
+            long permutation = 1;
+            for (long i = 1; i <= n; i++)
+            {
+                permutation = permutation * i;
+            }
+            return permutation;
+        }
+
+        public static long CalculateNumerator(long n, long k)
+        {
+            long numarator=1;
+            
+            for ( long i = k - 1 ; i >=0 ; i--)
+            {
+                numarator = numarator * (n - i);
+            }
+
+            return numarator;
+        }
+
+    }
+}
