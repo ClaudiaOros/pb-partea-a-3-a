bb76426a7e6f89696f47b7c34bf56e9fa31f336d
unknown <Administrator@claudia-pc.temasoft.pub>
2015-10-08 14:03:57 +0300

    Encryption

---
+++ PbPart3/EncryptionAndDecryption.Tests/EncryptionAndDecryptionTests.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using EncryptionAndDecryption;
+
+namespace EncryptionAndDecryption.Tests
+{
+    [TestClass]
+    public class EncryptionAndDecryptionTests
+    {
+        [TestMethod]
+        public void EncryptMessageNICAIERI_NU_E_CA_ACASA()
+        {
+            double numberOfColumns = 4;
+            string message = "nicaieri nu e ca acasa";
+            string expectedEncryptedMessage = "neeaircsciaaana0iuc0";
+
+            message = message.Replace(" ", "");
+            int messageLength = message.Length;
+            int numberOfLines = (int)Math.Ceiling(messageLength / numberOfColumns);
+
+            string encryptedMessage = EncryptiontAndDecryption.EncryptionAndDecryption.EncryptMessage(message, numberOfColumns, messageLength, numberOfLines);
+
+            int encryptedMessageLength = encryptedMessage.Length;
+            var charencryptedMessage = encryptedMessage.ToCharArray();
+            charencryptedMessage = ReplaceRandomLettersWith0(numberOfColumns, messageLength, numberOfLines, ref charencryptedMessage, encryptedMessageLength);
+
+           encryptedMessage = new string(charencryptedMessage);
+
+           Assert.AreEqual(expectedEncryptedMessage, encryptedMessage);
+        }
+
+        private static char[] ReplaceRandomLettersWith0(double numberOfColumns, int messageLength, int numberOfLines, ref char[] charencryptedMessage, int encryptedMessageLength)
+        {
+            int howManyLettersAreRandom = (int)(numberOfColumns * numberOfLines - messageLength);
+
+            int i = 0;
+            for (int j = encryptedMessageLength-1; j > 0; j--)
+            {
+
+                if (j % numberOfColumns == (numberOfColumns-1))
+                {
+                    charencryptedMessage[j] = '0';
+                    i++;
+                }
+                if (i == howManyLettersAreRandom)
+                {
+                    break;
+                }
+            }
+            return charencryptedMessage;
+        }
+    }
+}
---
+++ PbPart3/EncryptiontAndDecryption/EncryptionAndDecryption.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EncryptiontAndDecryption
+{
+    public class EncryptionAndDecryption
+    {
+        public static string EncryptMessage(string message, double numberOfColumns, int messageLength, int numberOfLines)
+        {
+            string encryptedMessage= "";
+
+            for (var i = 0; i < numberOfLines; i++)
+            {
+                for (var j = 0; j < numberOfColumns; j++)
+                {
+                    int nr = i + (j * numberOfLines);
+
+                    if (nr < messageLength)                  
+                        encryptedMessage = encryptedMessage + message[nr];                   
+                    else
+                    {
+                        var randomLetter = PickRandomLetter();
+                        encryptedMessage = encryptedMessage + randomLetter;
+                    }
+                }
+            }
+                return encryptedMessage;
+        }
+
+        public static string PickRandomLetter()
+        {
+            String[] alphabet = {
+                                        "A","B","C","D","E","F","G","H","I","J","K","L","M",
+                                        "N","O","P","Q","R","S","T","U","V","W","X","Y","Z",
+                                };
+
+            Random r = new Random();
+
+            int position = r.Next(0, 26);
+            string letter = alphabet[position];
+
+            return letter;
+        }
+    }
+}
