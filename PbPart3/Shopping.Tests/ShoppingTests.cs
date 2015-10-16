5ab259a333509f461f4973bc3ca9c74277a94f1c
unknown <Administrator@claudia-pc.temasoft.pub>
2015-10-16 15:21:12 +0300

    Shopping cart pb : total price

---
+++ PbPart3/Shopping.Tests/ShoppingTests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Shopping;
+
+namespace Shopping.Tests
+{
+    [TestClass]
+    public class ShoppingTests
+    {
+        public static Shopping.ShoppingCart[] cart = new Shopping.ShoppingCart[]
+          {
+              new Shopping.ShoppingCart { product = "bananaaaaas", price = 10 },
+              new Shopping.ShoppingCart { product = "apples", price = 3 },
+              new Shopping.ShoppingCart { product = "grapes", price = 4 },
+              new Shopping.ShoppingCart { product = "pears", price = 3 },
+              new Shopping.ShoppingCart { product = "oranges", price = 11 },
+              new Shopping.ShoppingCart { product = "lemons", price = 11 },
+              new Shopping.ShoppingCart { product = "applepie", price = 50 },
+              new Shopping.ShoppingCart { product = "cherries", price = 25 },
+          };
+
+        [TestMethod]
+        public void VerifyTotalPriceOfTheShoppingCart()
+        {
+            var expectedTotalPrice = 119;
+
+            var totalPrice = Shopping.CalculateTotatlPriceOfTheShoppingCart(cart);
+
+            Assert.AreEqual(expectedTotalPrice, totalPrice);
+        }
+
+        [TestMethod]
+        public void VerifyTheCheapestProductsFromTheShoppingCart()
+        {
+            Shopping.ShoppingCart[] cheapestProductsExpected = new Shopping.ShoppingCart[]
+            {
+                new Shopping.ShoppingCart { product = "apples", price = 3 },
+                new Shopping.ShoppingCart { product = "pears", price = 3 }
+            };
+
+            Shopping.ShoppingCart[] cheapestProducts = Shopping.ReturnCheapestProducts(cart);
+            CollectionAssert.AreEqual(cheapestProductsExpected, cheapestProducts);
+        }
+    }
+}
---
+++ PbPart3/Shopping/Shopping.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shopping
+{
+    public class Shopping
+    {
+        public struct ShoppingCart
+        {
+            public int price;
+            public string product;
+        }
+
+
+        public static int CalculateTotatlPriceOfTheShoppingCart(ShoppingCart[] cart)
+        {
+            int totalPrice = 0;
+
+            for (int i = 0; i < cart.Length; i++)
+            {
+                totalPrice += cart[i].price;
+            }
+
+            return totalPrice;
+        }
+
+        public static ShoppingCart[] ReturnCheapestProducts(ShoppingCart[] cart)
+        {
+            ShoppingCart[] cheapestProducts = new ShoppingCart[0];
+
+            int minPrice = 0;
+           // int productsWithMinValue = 0;
+
+            for (int i = 1; i < cart.Length; i++)
+            {
+                if (cart[i].price < minPrice)
+                {
+                    minPrice = cart[i].price;
+                }
+            }
+
+            for (int i = 0; i < cart.Length; i++)
+            {
+                if (cart[i].price == minPrice)
+                {
+                    //productsWithMinValue++;
+                    //Array.Resize(ref cheapestProducts, productsWithMinValue);
+                    cheapestProducts = new ShoppingCart[]
+                       {
+                           new ShoppingCart { price = cart[i].price, product = cart[i].product}
+                       };
+                }                  
+            }
+
+            return cheapestProducts;
+        }
+    }
+}
