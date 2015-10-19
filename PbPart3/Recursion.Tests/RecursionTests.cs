e9348269618ef6a29933b54ab64e5d27e5576b16
unknown <Administrator@claudia-pc.temasoft.pub>
2015-10-19 14:38:16 +0300

    started recursion pbs

---
+++ PbPart3/Recursion.Tests/RecursionTests.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Recursion;
+
+namespace Recursion.Tests
+{
+    [TestClass]
+    public class RecursionTests
+    {
+        [TestMethod]
+        public void Verify_Reverse_Of_String_ABCD()
+        {
+            string stringToBeReversed = "ABCD";
+            string expectedString = "DCBA";
+
+            var reverseString = Recursion.ReturnTheReverseString(stringToBeReversed);
+
+            Assert.AreEqual(expectedString, reverseString);
+        }
+
+        [TestMethod]
+        public void Verify_Reverse_Of_String_ANA_ARE_MERE_SI_PERE()
+        {
+            string stringToBeReversed = "ANA ARE MERE SI PERE";
+            string expectedString = "EREP IS EREM ERA ANA";
+
+            var reverseString = Recursion.ReturnTheReverseString(stringToBeReversed);
+
+            Assert.AreEqual(expectedString, reverseString);
+        }
+
+        [TestMethod]
+        public void Verify_The_Replacement_Of_A_Character_From_A_String_With_Another_String()
+        {
+            string firstString = "A";
+            string secondString = "pere";
+            int indexOfChar = 9;
+            string expectedString = "pere";
+
+            string resultedString = Recursion.ReplaceACharWithAString(indexOfChar, firstString, secondString);
+
+            Assert.AreEqual(expectedString, resultedString);
+        }
+
+        [TestMethod]
+        public void Verify_The_Replacement_Of_A_Character_From_A_String_With_Another_String_ANA_ARE_MERE()
+        {
+            string firstString = "ana are mere";
+            string secondString = "pere";
+            int indexOfChar = 9;
+            string expectedString = "ana are pereere";
+
+            string resultedString = Recursion.ReplaceACharWithAString(indexOfChar, firstString, secondString);
+
+            Assert.AreEqual(expectedString, resultedString);
+        }
+    }
+}
---
+++ PbPart3/Recursion/Recursion.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recursion
+{
+    public class Recursion
+    {
+        public static string ReturnTheReverseString(string s)
+        {
+
+            if ((s.Length == 1) || (String.IsNullOrEmpty(s))) return s;
+            else
+                return s[s.Length - 1] + ReturnTheReverseString(s.Substring(0, s.Length - 1));
+        }
+
+        public static string ReplaceACharWithAString(int index, string a, string b)
+        {
+            if (String.IsNullOrEmpty(a) || (a.Length == 1 && index == 1))
+                return b;
+            else
+                if (String.IsNullOrEmpty(b) || (b.Length == 1 && index == 1))
+                    return a;
+
+        }
+
+    }
+}
