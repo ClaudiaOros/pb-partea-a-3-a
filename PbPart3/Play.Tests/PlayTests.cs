6af1cd48a8c55741e85755847123265c8a7ef81a
unknown <Administrator@claudia-pc.temasoft.pub>
2015-10-23 10:18:16 +0300

    started play pb

---
+++ PbPart3/Play.Tests/PlayTests.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Play;
+
+namespace Play.Tests
+{
+    [TestClass]
+    public class PlayTests
+    {
+        [TestMethod]
+        public void Play1()
+        {
+            char[,] board = 
+            {
+               {' ',' ',' ',' '},
+               {' ',' ',' ',' '},
+               {' ','X',' ',' '},
+               {' ',' ',' ',' '},
+            };
+
+            var expectedMoves = 11;
+            var totalMoves = 0;
+
+            var moves = Play.ReturnMoves(board, 0, 0, ref totalMoves);
+
+            Assert.AreEqual(expectedMoves, totalMoves);
+        }
+
+        [TestMethod]
+        public void Play2()
+        {
+            char[,] board = 
+            {
+               {' ',' ',' ',' '},
+               {' ',' ',' ',' '},
+               {' ',' ',' ',' '},
+               {' ',' ',' ',' '},
+            };
+
+            var expectedMoves = 10;
+            var totalMoves = 0;
+
+            var moves = Play.ReturnMoves(board, 0, 0, ref totalMoves);
+
+            Assert.AreEqual(expectedMoves, totalMoves);
+        }
+
+        [TestMethod]
+        public void Play3()
+        {
+            char[,] board = 
+            {
+               {' ','X',' ','X'},
+               {' ',' ','X',' '},
+               {' ','X',' ',' '},
+               {'X',' ',' ','X'},
+            };
+
+            var expectedMoves = 0;
+            var totalMoves = 0;
+
+            var moves = Play.ReturnMoves(board, 0, 0, ref totalMoves);
+
+            Assert.AreEqual(expectedMoves, totalMoves);
+        }
+
+        [TestMethod]
+        public void Play4()
+        {
+            char[,] board = 
+            {
+               {'X',' ',' ','X'},
+               {' ',' ','X',' '},
+               {' ','X',' ',' '},
+               {'X',' ',' ','X'},
+            };
+
+            var expectedMoves = 0;
+            var totalMoves = 0;
+
+            var moves = Play.ReturnMoves(board, 0, 0, ref totalMoves);
+
+            Assert.AreEqual(expectedMoves, totalMoves);
+        }
+    }
+}
---
+++ PbPart3/Play/Play.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Play
+{
+    public class Play
+    {
+
+        public static int ReturnMoves(char[,] board, int row, int col, ref int totalMoves)
+        {
+            if ((col < 0) || (row < 0) ||
+
+        (col >= board.GetLength(1)) || (row >= board.GetLength(0)))
+            {              
+                return totalMoves;
+            }
+
+            if ((board[row, col] == ' ') && (board[row - 1, col] == ' ' || board[row + 1, col] == ' ' || board[row, col - 1] == ' ' || board[row, col + 1] == ' '))
+                totalMoves++;
+                        
+            totalMoves = ReturnMoves(board, row - 1, col, ref totalMoves);
+            totalMoves = ReturnMoves(board, row + 1, col, ref totalMoves);
+            totalMoves = ReturnMoves(board, row, col - 1, ref totalMoves);
+            totalMoves = ReturnMoves(board, row, col + 1, ref totalMoves);
+
+            return totalMoves;
+        }
+
+        public static bool NoMoreMoves(char[,] board)
+        {
+            var noMoreMoves = true;
+
+            for (int i = 0; i < board.GetLength(0); i++)
+                for (int j = 0; j < board.GetLength(1); j++)
+
+                    if ((board[i, j] == ' ') && (board[i - 1, j] == ' ' || board[i + 1, j] == ' ' || board[i, j - 1] == ' ' || board[i, j + 1] == ' '))
+                        noMoreMoves = false;
+
+            return noMoreMoves;
+        }
+
+    }
+}
