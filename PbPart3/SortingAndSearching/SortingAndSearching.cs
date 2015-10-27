f4c06271cd224f962cd1a8fd6c0eb8469ce3bad0
unknown <Administrator@claudia-pc.temasoft.pub>
2015-10-27 09:34:31 +0200

    first 3 pbs

---
+++ PbPart3/SortingAndSearching/SortingAndSearching.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortingAndSearching
+{
+    public class SortingAndSearching
+    {
+        public enum Priority
+        {
+            low,
+            medium,
+            high
+        };
+
+        public struct Reparation
+        {
+            public string reparation;
+            public Priority priority;
+            
+        }
+
+        public static string[] Quicksort(string[] text, int left, int right)
+        { 
+            int i = left, j = right;
+            string pivot = text[(left + right) / 2];
+
+            while (i <= j)
+            {
+                while (text[i].CompareTo(pivot) < 0)
+                {
+                    i++;
+                }
+
+                while (text[j].CompareTo(pivot) > 0)
+                {
+                    j--;
+                }
+
+                if (i <= j)
+                {
+                    // Swap
+                    string tmp = text[i];
+                    text[i] = text[j];
+                    text[j] = tmp;
+
+                    i++;
+                    j--;
+                }
+            }
+
+            // Recursive calls
+            if (left < j)
+            {
+                Quicksort(text, left, j);
+            }
+
+            if (i < right)
+            {
+                Quicksort(text, i, right);
+            }
+
+            return text;
+        }
+
+        public static string[] OrderDescending(string[] text)
+        {
+            string[] textToOrder = Quicksort(text, 0, text.Length - 1);
+
+            Array.Reverse(textToOrder);
+
+            return textToOrder;
+        }
+
+        public static int[] InsertionSortingAlgorithm(int[] array)
+        {
+            int temp,j;
+            for (int i = 1; i < array.Length; i++)
+            {
+                temp = array[i];
+                j = i - 1;
+
+                while (j >= 0 && array[j] > temp)
+                {
+                    array[j + 1] = array[j];
+                    j--;
+                }
+                array[j + 1] = temp;
+            }
+
+            return array;
+        }
+
+
+        public static Reparation[] SelectionSortingAlgorithm(Reparation[] repairs)
+        {
+            int min;
+            Reparation temp;
+
+            for (int i = 0; i < repairs.Length - 1; i++)
+            {
+                min = i;
+                for (int j = i + 1; j < repairs.Length; j++)
+                    if (repairs[j].priority < repairs[min].priority)
+                        min = j;
+
+               temp = SwapValues(repairs, min, i);
+            }
+
+            return repairs;
+        }
+
+        private static Reparation SwapValues(Reparation[] repairs, int min, int i)
+        {
+                Reparation temp;
+               
+                temp = repairs[i];
+                repairs[i] = repairs[min];
+                repairs[min] = temp;
+                return temp;
+        }
+
+        
+        public static void Swap(string[] array, int a, int b)
+        {
+            string temp = array[a];
+            array[a] = array[b];
+            array[b] = temp;
+        }
+
+        public static string[] QuickSort3Algorithm(string[] text, int left, int right)
+        {
+ 
+            // choose pivot
+                string pivot = text[(left + right) / 2];
+
+            // 3-way partition
+              int i = 1;
+              int k = 1;
+              int n = text.Length;
+              int p = n;
+
+
+            //    while (i < p)
+            //    {
+            //     if ( (int)text[i] < (int)text[n])
+            //        Swap(text,i++,k++);                 
+            //     else 
+            //         if (text[i] == text[n])                         
+            //             Swap(text,i,p--);                   
+            //     else 
+            //             i++;
+            //    }
+
+
+            //// move pivots to center
+            //   int m = GetMinValue(p-k,n-p+1);
+            //    Swap a[k..k+m-1,n-m+1..n]
+
+            // recursive sorts
+            
+            QuickSort3Algorithm(text,1,k-1);
+
+           QuickSort3Algorithm(text,n-p+k+1,n);
+
+            return text;
+
+        }
+
+        public static int GetMinValue(int a, int b)
+        {
+            return  a < b ? a:b;
+
+        }
+
+    }
+}
---
+++ PbPart3/SortingAndTesting.Tests/SortingAndSearchingTests.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SortingAndSearching;
+
+namespace SortingAndTesting.Tests
+{
+    [TestClass]
+    public class SortingAndSearchingTests
+    {
+        [TestMethod]
+        public void Ascending_order_of_a_text()
+        {
+            string[] text = {"ana","are","mere","pere","gutui","kiwi" };
+            string[] expectedAscendingSortedText = { "ana","are","gutui","kiwi","mere","pere" };
+
+            string[] ascendingText = SortingAndSearching.SortingAndSearching.Quicksort(text,0,text.Length-1);
+            
+            CollectionAssert.AreEqual(expectedAscendingSortedText, ascendingText,"Ascending order not correct");
+        }
+
+        [TestMethod]
+        public void Descending_order_of_a_text()
+        {
+            string[] text = { "ana", "are", "mere", "pere", "gutui", "kiwi" };
+            string[] expectedDescendingSortedText = { "pere", "mere", "kiwi", "gutui", "are", "ana" };
+
+            string[] descendingText = SortingAndSearching.SortingAndSearching.OrderDescending(text);
+
+            CollectionAssert.AreEqual(expectedDescendingSortedText, descendingText, "Descending order not correct");
+        }
+
+        [TestMethod]
+        public void Ascending_loto_numbers()
+        {
+            int[] numbers = { 2, 5, 34, 23, 8, 13 };
+            int[] expectedAscendingNumbers = { 2, 5, 8, 13, 23, 34 };
+
+            int[] ascendingNumbers = SortingAndSearching.SortingAndSearching.InsertionSortingAlgorithm(numbers);
+
+            CollectionAssert.AreEqual(expectedAscendingNumbers, ascendingNumbers);
+        }
+
+        [TestMethod]
+        public void Reparation_Priority()
+        {
+            SortingAndSearching.SortingAndSearching.Reparation[]  reparations = new SortingAndSearching.SortingAndSearching.Reparation[]
+             {  
+                 new SortingAndSearching.SortingAndSearching.Reparation
+                 {
+                     reparation = "schimb anvelope" ,
+                     priority = SortingAndSearching.SortingAndSearching.Priority.low                      
+                 },
+
+                 new SortingAndSearching.SortingAndSearching.Reparation
+                 {
+                     reparation = "schimb ulei" ,
+                     priority =SortingAndSearching.SortingAndSearching.Priority.medium                      
+                 },
+                  new SortingAndSearching.SortingAndSearching.Reparation
+                 {
+                     reparation = "schimb motor" ,
+                     priority = SortingAndSearching.SortingAndSearching.Priority.high                      
+                 },
+                  new SortingAndSearching.SortingAndSearching.Reparation
+                 {
+                     reparation = "schimb stergatoare" ,
+                     priority = SortingAndSearching.SortingAndSearching.Priority.low                      
+                 },
+                  new SortingAndSearching.SortingAndSearching.Reparation
+                 {
+                     reparation = "spalat motor" ,
+                     priority = SortingAndSearching.SortingAndSearching.Priority.medium                      
+                 },
+                  new SortingAndSearching.SortingAndSearching.Reparation
+                 {
+                     reparation = "vopsit" ,
+                     priority = SortingAndSearching.SortingAndSearching.Priority.medium                      
+                 }
+             };
+
+            var expectedReparationsOrderedByPriority = new SortingAndSearching.SortingAndSearching.Reparation[]
+            {
+                new SortingAndSearching.SortingAndSearching.Reparation
+                 {
+                     reparation = "schimb anvelope" ,
+                     priority = SortingAndSearching.SortingAndSearching.Priority.low                      
+                 },
+                  new SortingAndSearching.SortingAndSearching.Reparation
+                 {
+                     reparation = "schimb stergatoare" ,
+                     priority = SortingAndSearching.SortingAndSearching.Priority.low                      
+                 },
+                  new SortingAndSearching.SortingAndSearching.Reparation
+                 {
+                     reparation = "schimb ulei" ,
+                     priority =SortingAndSearching.SortingAndSearching.Priority.medium                      
+                 },
+                  new SortingAndSearching.SortingAndSearching.Reparation
+                 {
+                     reparation = "spalat motor" ,
+                     priority = SortingAndSearching.SortingAndSearching.Priority.medium                      
+                 },
+                
+                   new SortingAndSearching.SortingAndSearching.Reparation
+                 {
+                     reparation = "vopsit" ,
+                     priority = SortingAndSearching.SortingAndSearching.Priority.medium                      
+                 },
+                   new SortingAndSearching.SortingAndSearching.Reparation
+                 {
+                     reparation = "schimb motor" ,
+                     priority = SortingAndSearching.SortingAndSearching.Priority.high                      
+                 }
+            };
+
+            var reparationsOrderedByPriority = SortingAndSearching.SortingAndSearching.SelectionSortingAlgorithm(reparations);
+
+            CollectionAssert.AreEqual(expectedReparationsOrderedByPriority, reparationsOrderedByPriority);
+        }
+        
+    }
+}
