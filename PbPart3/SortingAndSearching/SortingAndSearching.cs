cce78c41cde6cdb5038509a653bbb6f241ab8a57
unknown <Administrator@claudia-pc.temasoft.pub>
2015-11-03 15:21:30 +0200

    some code for vector

--- PbPart3/SortingAndSearching/SortingAndSearching.cs
+++ PbPart3/SortingAndSearching/SortingAndSearching.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Linq;
 
 namespace SortingAndSearching
 {
@@ -39,7 +38,6 @@ namespace SortingAndSearching
             public string word;
             public int occur;
         }
-
         
         public struct Subjects
         {
---
+++ PbPart3/Vector/VectorPb.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vector
+{
+    public class VectorPb: IEnumerable, IEnumerator
+    {
+        private object Current 
+        {
+            get; 
+        }
+
+        private  bool MoveNext()
+        {
+            return true;
+        }
+
+        private void Reset()
+        {
+ 
+        }
+        
+    }
+}
