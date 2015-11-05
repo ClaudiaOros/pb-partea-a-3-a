ec8270d74564490867d5a96539d904d11dbae437
unknown <Administrator@claudia-pc.temasoft.pub>
2015-11-05 09:01:48 +0200

    Added Tests for Insert Method

--- PbPart3/VectorPb.Tests/VectorPbTests.cs
+++ PbPart3/VectorPb.Tests/VectorPbTests.cs
@@ -35,5 +35,54 @@ namespace VectorPb.Tests
 
             CollectionAssert.AreEqual(expectedVector, resultedVector);
         }
+
+        [TestMethod]
+        public void Verify_an_array_after_adding_a_new_element_on_a_position_where_is_no_element()
+        {
+            int count = 7;
+            object[] obj = new object[8] { 1, 2, 3, 4, 5, 6, 7, null };
+            var expectedVector = new object[] { 1, 2, 3, 4, 5, 6, 7, 9 };
+            var vector = new Vector.VectorPb(obj, count);
+
+            object elementToBeAdded = 9;
+            int position = 7;
+
+            var resultedVector = vector.Insert(position, elementToBeAdded);
+
+            CollectionAssert.AreEqual(expectedVector, resultedVector);
+        }
+
+        [TestMethod]
+        public void Verify_an_array_after_adding_a_new_element_on_a_position_where_is_an_element_without_resizing_array()
+        {
+            int count = 7;
+            object[] obj = new object[8] { 1, 2, 3, 4, 5, 6, 7, null };
+            var expectedVector = new object[] { 1, 2, 3, 9, 4, 5, 6, 7 };
+            var vector = new Vector.VectorPb(obj, count);
+
+            object elementToBeAdded = 9;
+            int position = 3;
+
+            var resultedVector = vector.Insert(position, elementToBeAdded);
+
+            CollectionAssert.AreEqual(expectedVector, resultedVector);
+        }
+
+        [TestMethod]
+        public void Verify_an_array_after_adding_a_new_element_on_a_position_where_is_an_element_with_resizing_array()
+        {
+            int count = 8;
+            object[] obj = new object[8] { 1, 2, 3, 4, 5, 6, 7, 8 };
+            var expectedVector = new object[] { 1, 2, 3, 9, 4, 5, 6, 7, 8, null, null, null, null, null, null, null };
+            var vector = new Vector.VectorPb(obj, count);
+
+            object elementToBeAdded = 9;
+            int position = 3;
+
+            var resultedVector = vector.Insert(position, elementToBeAdded);
+
+            CollectionAssert.AreEqual(expectedVector, resultedVector);
+        }
+            
     }
 }
