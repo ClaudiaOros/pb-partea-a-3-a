c37a94622b7e795ad46d7a0dc8341b6cfda06947
unknown <Administrator@claudia-pc.temasoft.pub>
2015-11-05 08:30:53 +0200

    added method Add and tests for it

--- PbPart3/Vector/VectorPb.cs
+++ PbPart3/Vector/VectorPb.cs
@@ -6,23 +6,66 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace Vector
-{
-    public class VectorPb: IEnumerable, IEnumerator
+{  
+
+    public class VectorPb  //: IEnumerable, IEnumerator
     {
-        private object Current 
+
+        public int count;
+        public object[] obj;
+
+        public VectorPb (object[] obj, int count)
         {
-            get; 
+            this.count = count;
+            this.obj = obj;
         }
 
-        private  bool MoveNext()
-        {
-            return true;
-        }
+            public object[] Add(object element)
+            {
+                if (count % 8 == 0)
+                {
+                    var obj2 = new object[count*2];
+                    
+                    for (int i = 0 ; i< count; i++)
+                    {
+                        obj2[i] = obj[i];
+                    }
+                    obj2[count] = element;
+                    return obj2;
+                }
+                else 
+                {              
+                    obj[count] = element;
+                    return obj;
+                }                
+            }
 
-        private void Reset()
-        {
- 
-        }
+            public object[] Insert(int position, object element)
+            {
+                
+                return obj;
+            }
+
+            public object[] Remove(object element)
+            {
+
+                return obj;
+            }
+
+            public object[] Remove(int index)
+            {
+                return obj;
+            }
+
+            private bool MoveNext()
+            {
+                return true;
+            }
+
+            private void Reset()
+            {
+
+            }
         
     }
 }
---
+++ PbPart3/VectorPb.Tests/VectorPbTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using VectorPb;
+using Vector;
+
+namespace VectorPb.Tests
+{
+    [TestClass]
+    public class VectorPbTests
+    {
+        [TestMethod]
+        public void Verify_an_array_after_adding_a_new_element_No_Resizing_the_aray()
+        {
+            int count = 7;
+            object[] obj = new object[8] { 1, 2, 3, 4, 5, 6, 7, null };
+            var expectedVector = new object[] { 1, 2, 3, 4, 5, 6, 7, 9 };
+            var vector = new Vector.VectorPb(obj,count);
+
+            object elementToBeAdded = 9;
+            var resultedVector = vector.Add(elementToBeAdded);
+
+            CollectionAssert.AreEqual(expectedVector, resultedVector);
+        }
+
+        [TestMethod]
+        public void Verify_an_array_after_adding_a_new_element_With_Resizing_the_array_of_8_elements()
+        {
+            int count = 8;
+            object[] obj = new object[8] { 1, 2, 3, 4, 5, 6, 7, 8 };
+            var expectedVector = new object[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 ,null,null,null,null,null,null,null};
+            var vector = new Vector.VectorPb(obj, count);
+
+            object elementToBeAdded = 9;
+            var resultedVector = vector.Add(elementToBeAdded);
+
+            CollectionAssert.AreEqual(expectedVector, resultedVector);
+        }
+    }
+}
